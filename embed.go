70740dc0267b9c77566798c969c3f6f23312e05e
Jeff Keen <jeff@keen.me>
2026-08-12 19:46:39 -0500

    feat: add glossary to define terms

--- embed.go
+++ embed.go
@@ -4,3 +4,8 @@ import "embed"
 
 //go:embed couriers/*
 var Couriers embed.FS
+
+// Glossary says what each named part of a number is, for every format that has one.
+//
+//go:embed glossary.json
+var Glossary embed.FS
